503c2cdf19ce86571c025c462965a2d54b0a8184
Marnes <marnick.leau@gmail.com>
2017-11-20 20:33:16 +0100

    remove pointless wrapping

--- background/initialize.js
+++ background/initialize.js
@@ -1,32 +1,26 @@
 "use strict";
 
-(() => {
-	chrome.storage.sync.get({
-		popupSuccess: null,
-		popupFail: null
-	}, options => {
-		const overwrite = {};
+chrome.storage.sync.get({
+	popupSuccess: null,
+	popupFail: null
+}, options => {
+	const overwrite = {};
 
-		//
+	if (typeof(options.popupSuccess) !== "boolean") {
+		overwrite.popupSuccess = false;
+	}
 
-		if (typeof(options.popupSuccess) !== "boolean") {
-			overwrite.popupSuccess = false;
-		}
+	if (typeof(options.popupFail) !== "boolean") {
+		overwrite.popupFail = true;
+	}
 
-		if (typeof(options.popupFail) !== "boolean") {
-			overwrite.popupFail = true;
-		}
-
-		//
-
-		const keys = Object.keys(overwrite);
-		if (keys.length > 0) {
-			chrome.storage.sync.set(overwrite, () => {
-				chrome.runtime.openOptionsPage();
-				main();
-			});
-		} else {
+	const keys = Object.keys(overwrite);
+	if (keys.length > 0) {
+		chrome.storage.sync.set(overwrite, () => {
+			chrome.runtime.openOptionsPage();
 			main();
-		}
-	});
-})();
+		});
+	} else {
+		main();
+	}
+});
--- options/options.js
+++ options/options.js
@@ -1,30 +1,24 @@
 "use strict";
 
-(() => {
-	const popupSuccessCheckbox = document.getElementById("popupSuccess");
-	const popupFailCheckbox = document.getElementById("popupFail");
+const popupSuccessCheckbox = document.getElementById("popupSuccess");
+const popupFailCheckbox = document.getElementById("popupFail");
 
-	//
+chrome.storage.sync.get({
+	popupSuccess: null,
+	popupFail: null,
+}, options => {
+	popupSuccessCheckbox.checked = options.popupSuccess;
+	popupFailCheckbox.checked = options.popupFail;
 
-	chrome.storage.sync.get({
-		popupSuccess: null,
-		popupFail: null,
-	}, options => {
-		popupSuccessCheckbox.checked = options.popupSuccess;
-		popupFailCheckbox.checked = options.popupFail;
-
-		//
-
-		popupSuccessCheckbox.addEventListener("change", event => {
-			chrome.storage.sync.set({
-				popupSuccess: popupSuccessCheckbox.checked
-			});
-		});
-
-		popupFailCheckbox.addEventListener("change", event => {
-			chrome.storage.sync.set({
-				popupFail: popupFailCheckbox.checked
-			});
+	popupSuccessCheckbox.addEventListener("change", event => {
+		chrome.storage.sync.set({
+			popupSuccess: popupSuccessCheckbox.checked
 		});
 	});
-})();
+
+	popupFailCheckbox.addEventListener("change", event => {
+		chrome.storage.sync.set({
+			popupFail: popupFailCheckbox.checked
+		});
+	});
+});
