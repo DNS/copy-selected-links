a549a82f68cdf59bfd756d21853ad7d261f8821e
Marnes <marnick.leau@gmail.com>
2020-01-09 22:01:00 +0100

    only trigger update listener on addon changes

--- ts/background/SettingsValidator.ts
+++ ts/background/SettingsValidator.ts
@@ -1,18 +1,21 @@
-import {browser} from "webextension-polyfill-ts";
+import {browser, Runtime} from "webextension-polyfill-ts";
 import {Settings} from "../common/Settings";
+import OnInstalledDetailsType = Runtime.OnInstalledDetailsType;
 
 export class SettingsValidator {
 	public static guard() {
 		browser.runtime.onInstalled.addListener(SettingsValidator.onInstall);
 	}
 
-	private static onInstall() {
-		browser.storage.sync.get(Settings.shim()).then((data: any) => {
-			const settings = Settings.parse(data);
+	private static onInstall(details: OnInstalledDetailsType) {
+		if (details.reason === "install"  || details.reason === "update") {
+			browser.storage.sync.get(Settings.shim()).then((data: any) => {
+				const settings = Settings.parse(data);
 
-			if (!settings.equals(data)) {
-				settings.save().then(browser.runtime.openOptionsPage.bind(browser.runtime));
-			}
-		});
+				if (!settings.equals(data)) {
+					settings.save().then(browser.runtime.openOptionsPage.bind(browser.runtime));
+				}
+			});
+		}
 	}
 }
