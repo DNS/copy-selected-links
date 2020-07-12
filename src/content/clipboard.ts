d018f2866e4ca076d1fb2096f6c7e3615b3b6e0a
Marnes <marnick.leau@gmail.com>
2020-07-12 17:28:33 +0200

    #25 keep clipboard event mutation inside eventlistener context

--- src/content/clipboard.ts
+++ src/content/clipboard.ts
@@ -1,16 +1,19 @@
 // https://github.com/mdn/webextensions-examples/blob/master/context-menu-copy-link-with-types/clipboard-helper.js
 export async function copyToClipboard(text: string): Promise<void> {
-    const copied = await new Promise<ClipboardEvent>(resolve => {
+    await new Promise<ClipboardEvent>(resolve => {
         const oncopy = (event: ClipboardEvent): void => {
             document.removeEventListener("copy", oncopy, true);
             event.stopImmediatePropagation();
             event.preventDefault();
-            resolve(event);
+
+            if (event.clipboardData == null) {
+                throw new Error("missing clipboard context?");
+            }
+            event.clipboardData.setData("text/plain", text);
+            resolve();
         };
 
         document.addEventListener("copy", oncopy, true);
         document.execCommand("copy");
     });
-
-    (copied.clipboardData as DataTransfer).setData("text/plain", text);
 }
