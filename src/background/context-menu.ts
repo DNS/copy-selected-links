21339056be29c567f9bff3d3f110dddfbfb6e8a3
Marnes <marnick.leau@gmail.com>
2020-09-11 22:59:53 +0200

    promisify context menu function

--- src/background/context-menu.ts
+++ src/background/context-menu.ts
@@ -3,22 +3,19 @@ import {arrangeCopy} from "./copy";
 
 const CONTEXT_MENU_ID = "copySelectedLinks_CopySelectedLinks";
 
-function checkError(): void {
-    if (browser.runtime.lastError == null) {
-        return;
-    }
-
-    const errorMsg = browser.runtime.lastError.message;
-
-    if (errorMsg === `Cannot create item with duplicate id ${CONTEXT_MENU_ID}`) {
-        // ignore
-        // TODO find some kind of one-off trigger to register contextmenu
-    } else {
-        throw new Error(errorMsg);
-    }
+async function createContextMenu(options: Menus.CreateCreatePropertiesType): Promise<void> {
+    return new Promise((resolve, reject) => {
+        browser.contextMenus.create(options, (): void => {
+            if (browser.runtime.lastError == null) {
+                resolve();
+            } else {
+                reject(new Error(browser.runtime.lastError.message));
+            }
+        });
+    });
 }
 
-async function onClick(contextMenuInfo: Menus.OnClickData, tab?: Tabs.Tab): Promise<void> {
+async function signalTabForCopying(contextMenuInfo: Menus.OnClickData, tab?: Tabs.Tab): Promise<void> {
     if (tab == null) {
         throw new Error("invoked context menu without a tab?");
     }
@@ -33,16 +30,13 @@ async function onClick(contextMenuInfo: Menus.OnClickData, tab?: Tabs.Tab): Prom
 }
 
 export function registerMenu(): void {
-    browser.contextMenus.onClicked.addListener((data, tab) => void onClick(data, tab).catch(console.error));
+    browser.contextMenus.onClicked.addListener((data, tab) => void signalTabForCopying(data, tab).catch(console.error));
 
-    browser.contextMenus.create(
-        {
-            contexts: ["selection", "link"],
-            documentUrlPatterns: ["*://*/*", "file:///*"],
-            id: CONTEXT_MENU_ID,
-            title: "Copy selected links",
-            type: "normal"
-        },
-        checkError
-    );
+    createContextMenu({
+        contexts: ["selection", "link"],
+        documentUrlPatterns: ["*://*/*", "file:///*"],
+        id: CONTEXT_MENU_ID,
+        title: "Copy selected links",
+        type: "normal"
+    }).catch(console.error);
 }
